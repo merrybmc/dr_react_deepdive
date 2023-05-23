d8c09c915697b15ab52ee3755f13797079ed7aa8
Byeong Min Cho <merrybmc@gmail.com>
2023-05-24 00:52:58 +0900

    context

---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,9 @@
+import { useState, createContext } from 'react';
+
+export const DarkModeContext = createContext();
+
+export function DarkModeProvider({ children }) {
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => setDarkMode(!darkMode);
+  return <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>{children}</DarkModeContext.Provider>;
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './AppForm';
+import App from './AppTheme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
